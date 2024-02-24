3e0797653baa73387a357f4fe8879f5b64f4ca48
Manahil017 <160819091+Manahil017@users.noreply.github.com>
2024-02-24 05:40:48 +0000

    variable done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID = 112233
+let accountEmail = "manahil@google.com"
+var accountPassword = "1234"
+acoountCity = "Jhang"
+
+console.table([accountID,accountEmail,accountPassword,acoountCity])
